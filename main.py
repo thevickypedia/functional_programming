1d9b8b39628b34d0e9d12b1e9d274680ef989a7d
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-07-31 10:42:48 -0500

    added inline comments

--- main.py
+++ main.py
@@ -7,21 +7,27 @@ start_process = time.time()
 
 
 def process_function():
-    for _ in range(50000000):
+    for _ in range(50000000):  # runs loops for 50 million times
         alpha = 0
 
 
-processes = []
+processes = []  # creates an empty list for processes
 
-for _ in range(os.cpu_count()):
-    processes.append(Process(target=process_function))
+# the loop below appends the processes of how many times the function should be executed (number of CPUs in this case).
+for _ in range(os.cpu_count()):  # _ is a throw away variable here as I'm going to use it anywhere in the loop
+    processes.append(Process(target=process_function))  # **
+# ** Note that we just pass the function unexecuted as we do not want to execute the function without multiprocessing
 
+# starts the function as many times as in the processes
 for process in processes:
-    process.start()
+    process.start()  # starts each process in the processes list
 
+# finishes all the processes
+# without using join() here, the code will continue to execute without finishing all the processes
 for process in processes:
     process.join()
 
+# prints the run time using multiprocessing
 print(f'Process execution time: {round(float(time.time() - start_process), 2)}')
 
 
@@ -29,19 +35,25 @@ start_thread = time.time()
 
 
 def thread_function():
-    for _ in range(50000000):
+    for _ in range(50000000):  # runs loops for 50 million times
         beta = 0
 
 
-threads = []
+threads = []  # creates an empty list for threads
 
-for _ in range(os.cpu_count()):
-    threads.append(Thread(target=thread_function))
+# the loop below appends the threads of how many times the function should be executed (number of CPUs in this case).
+for _ in range(os.cpu_count()):  # _ is a throw away variable here as I'm going to use it anywhere in the loop
+    threads.append(Thread(target=thread_function))  # **
+# ** Note that we just pass the function unexecuted as we do not want to execute the function without multithreading
 
+# starts the function as many times as in the threads
 for thread in threads:
-    thread.start()
+    thread.start()  # starts each process in the processes list
 
+# finishes all the threads
+# without using join() here, the code will continue to execute without finishing all the threads
 for thread in threads:
     thread.join()
 
+# prints the run time using multithreading
 print(f'Thread execution time: {round(float(time.time() - start_thread), 2)}')
